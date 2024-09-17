1ef77977c1f698a743c4dfce543ae09a88612b66
richardxavierpious <richardpious369@gmail.com>
2024-09-17 12:45:53 +0530

    intial

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <img src='/logo.svg' width={150} height={100}/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/ui/Header'
+
+function Home() {
+  return (
+    <div>
+        {/* Header */}
+        <Header/>
+        {/* Hero */}
+    </div>
+  )
+}
+
+export default Home 
